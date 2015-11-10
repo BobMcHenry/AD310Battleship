ee92f91338d3b401db049be1e118e0736df6af7b
mariorodriguezdeleon <mariorodriguezdeleon@gmail.com>
2015-11-09 23:14:27 -0800

    Java Docs
    Added the java documentation for the methods

--- BattleshipModel.java
+++ BattleshipModel.java
@@ -10,20 +10,69 @@
 // @TODO Mario - Add Javadoc
 public interface Battleship {
 
+    /**
+    *Returns one of the two game modes of play.
+    *@return the current game mode
+    */
     public PlayMode getPlayMode();
-    public PlayMode setPlayMode(PlayMode playMode);
 
+    /**
+    *Sets the play mode from setup to battle and back
+    *@param PlayMode public
+    *@return the play mode that was implemented
+    */
+    playMode PlayMode setPlayMode(PlayMode playMode);
+
+    /**
+    *Returns the player that is currently on the offensive
+    *@return the player that is currently active
+    */
     public Player getActivePlayer();
+
+    /**
+    *Sets the specified player for offensive play
+    *@param Player player
+    */
     public void setActivePlayer(Player player);
 
+    /**
+    *resets the gameboard, ships and counters once game is over
+    */
     public void resetGame();
 
+    /**
+    *Returns a string representation of 
+    *the specified player
+    *@param int player
+    *@return current state of board from specified player
+    */
     public String getPlayerBoard(int player);
 
+    /**
+    *Returns true if a ship is succefully placed on the board
+    *@param int player
+    *@param ShipInterface ship
+    *@param Location head
+    *@param Location tial
+    *@return true if the ship is succefully placed
+    */
     public boolean placeShip(int player, ShipInterface ship, Location head, Location tail);
 
+
+    /**
+    *Returns the location of the sip of specified player, head and tial.
+    *@param int player
+    *@param ShipType ship
+    *@return the location of the specifed ship from the specified player
+    */
     public Location getShip(int player, ShipType ship);
 
+    /**
+    *Returns the result of the shot fired by the specified player, i.e. hit or miss
+    *@param int player
+    *@param Location target
+    *@return the resuld of the shot fired by the specified player at the specified location
+    */
     public ShotResult makeShot(int player, Location target);
 
 }
