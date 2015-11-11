f61294c8076e3af32e60f1580407910294887241
Bob McHenry <bobmchenryvideo@gmail.com>
2015-11-11 08:53:37 -0800

    Player helper class added.

---
+++ src/battleship/model/Player.java
@@ -0,0 +1,20 @@
+package battleship.model;
+
+class Player{
+    String name;
+    Board pBoard;
+    
+    Player(String playerName, Board playerBoard){
+        this.name = playerName;
+        this.pBoard = playerBoard;
+    }
+
+    Board getBoard(){
+        return pBoard;
+    }
+
+    String getName(){
+        return name;
+    }
+
+}
