7fbf4d3e9ad4e3a352e240d5e13e84553a59ae68
Bob McHenry <bobmchenryvideo@gmail.com>
2015-11-28 23:04:49 -0800

    Added javadoc to Location class.

--- src/battleship/model/Location.java
+++ src/battleship/model/Location.java
@@ -2,21 +2,30 @@ package battleship.model;
 
 /**
  * A class that represents the status of one square on the playing board.
- * Uses a char,int pair as the coordinate system where char maps to the row, and int maps to the column
- * The column numbers increase as you move left, the rows decrease with J being the bottom most row.
+ * Uses a row,col pair as the coordinate system, rows and cols numbered 0-9
  */
 class Location {
+    /**
+     * Row position of Location
+     */
     private final int row;
+    /**
+     * Column position of Location
+     */
     private final int column;
+    /**
+     * Status enum value. HIT, MISS, or INITIAL
+     */
     private Status state;
 
-    /*
+    /**
      * Class constructor, constructs a Location from a char, int pair.
-     * @param r The row of this location, valid values are A - J and a - j.
-     * @param c The column of this location, valid values are 1 - 10.
+     * @param r The row of this location, valid values are 0-9.
+     * @param c The column of this location, valid values are 0-9.
      * @throws IllegalArgumentException if either r or c are out of range.
      */
     Location(int r, int c) {
+        //Validation
         if (r < 0 || r > 9) {
             throw new IllegalArgumentException(
                 "Row out of range: " + r + ", expected range 0 through 9 ");
@@ -25,43 +34,59 @@ class Location {
             throw new IllegalArgumentException(
                 "Column out of range: " + c + ", expected range 0 through 9 ");
         }
+        //Assignment
         this.row = r;
         this.column = c;
         state = Status.INITIAL;
     }
 
-    /*
+    /**
      * Returns the Row of this location.
+     * @return the int value of the row this Location occupies
      */
     int getRow() {
         return row;
     }
 
-    /*
+    /**
      * Returns the Column of this location.
+     * @return the int value of the column this Location occupies.
      */
     int getColumn() {
         return column;
     }
 
+    /**
+     * Returns the calculated index value of the location.
+     * @return int equal to (row * width + column)
+     */
     int getIndex(){ return row*10 + column; }
 
-    /*
-     * Returns the Status of this location.
+    /**
+     * Returns the Status of this location. HIT, MISS, or INITIAL
+     * @return Status enum type assigned to location.
+     * HIT if fired on and is in a Ship's Location[]
+     * MISS if fired on and is not in a Ship's Location[]
+     * or INITIAL if not yet fired on.
      */
     Status getStatus() {
         return state;
     }
 
-    /*
+    /**
      * Sets the status of this location.
+     * @param value The new Status enum type of location
      */
     void setStatus(Status value) {
         state = value;
     }
 
+    /**
+     * String representation of Location object
+     * @return Information string in the format Row: # | Column: # | State: STATUS
+     */
     @Override
     public String toString(){
-        return "Row: " + row + " Column: " + column + " State: " + state.toString() + "\n";
+        return "Row: " + row + " | Column: " + column + " | State: " + state.toString() + "\n";
     }
 }
